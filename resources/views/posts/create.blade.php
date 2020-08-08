658b652c012f22761731f63d0e63b96368e93225
f-chilmi <fchilmi5@gmail.com>
2020-08-08 23:33:50 +0700

    create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 <div class="ml-3 mt-3 mr-3">
 <div class="card card-primary">
     <div class="card-header">
-        <h3 class="card-title">Create New Post</h3>
+        <h3 class="card-title">Create New Questions</h3>
     </div>
     <!-- /.card-header -->
     <!-- form start -->
@@ -19,7 +19,29 @@
                 <label for="body">Body</label>
                 <input type="text" class="form-control" id="body" name="body" placeholder="Body">
             </div>
+            <div class="form-group">
+                <label>Created at</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                    </div>
+                    <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" im-insert="false">
+                </div>
+                  <!-- /.input group -->
+            </div>
+            <div class="form-group">
+                <label>Updated at</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                    </div>
+                    <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" im-insert="false">
+                  </div>
+                  <!-- /.input group -->
+                </div>
+            </div>
         </div>
+            
             
             <!-- /.card-body -->
 
